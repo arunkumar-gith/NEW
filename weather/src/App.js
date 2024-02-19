d47f94107928afa8ecf190d9ad814a5c2b3727e1
Arun <kumar.arun52244@gmail.com>
2024-02-19 11:46:17 +0530

    new commit

---
+++ weather/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './index.css';
+import Myapp from './Main';
+function App() {
+  return (
+    <div className="App">
+      <>
+       <h1 className='print'>Current Weather</h1>
+     
+      <Myapp/>
+     
+      </>
+    </div>
+  );
+}
+
+export default App;
